1ebc7ee96f18605edc690c5c19ae5fec0a82f5d7
ugaleanuja0617@gmail.com <ugaleanuja0617@gmail.com>
2026-09-12 10:01:51 +0530

    add file

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useAuth } from '../context/AuthContext';
 import { ThemeToggle } from '../components/common/ThemeToggle';
+import { apiClient } from "@/lib/apiClient";
 import {
   Shield,
   FolderLock,
